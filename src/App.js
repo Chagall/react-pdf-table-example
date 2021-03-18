570ff3acddae0eb4eece82d390c5395598a018d8
Fábio Alves Martins Pereira <famp@operamail.com>
2021-03-18 14:53:09 -0300

    Initial project setup, with create-react-app and the react-pdf library already added.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Project created!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
